f8b6a7246d997e24236b7e294f7d65f5e87b1fae
Khrystyna2 <l.xrustja@gmail.com>
2019-01-03 16:57:06 +0200

    added new page(article), fixed header

--- js/main-js.js
+++ js/main-js.js
@@ -16,6 +16,20 @@ $(document).ready(function() {
     $('.menu-btn').on('click', function() {
 		$(this).toggleClass('menu-btn_active');
 		$('.top-menu').slideToggle();
-	});
+    });
+    
+    //fixed menu
+    $(window).scroll(function(){
+        let winTop = $(window).scrollTop();
+        let heightMenu = $('.fixed-header').height();
+        let heightHeader = $('.fixed-header').outerHeight(true);
+        if(winTop >= heightMenu){
+        $(".fixed-header").addClass("sticky");
+        $('.header').css('padding-top', heightHeader);
+        }else{
+        $(".fixed-header").removeClass("sticky");
+        $('.header').css('padding-top', 0);
+        }
+    });
 
 });
